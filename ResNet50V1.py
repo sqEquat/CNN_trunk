c657f0205318010c0ee83fff02df45a7f1f6d584
Equat <equati@yandex.ru>
2021-03-21 22:15:07 +0300

    Add proper preprocessing

--- ResNet50V1.py
+++ ResNet50V1.py
@@ -3,20 +3,22 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications.resnet import preprocess_input
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Input, GlobalAveragePooling2D, Dropout, Dense
 
 
 # Disable training on GPU
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-train_datagen = image.ImageDataGenerator(width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2,
-                                         zoom_range=0.2, vertical_flip=True)
+batch_size = 8
+
+train_datagen = image.ImageDataGenerator(width_shift_range=0.3, height_shift_range=0.3, shear_range=0.1,
+                                         vertical_flip=True, preprocessing_function=preprocess_input)
 
 test_datagen = image.ImageDataGenerator()
 
-train_generator = train_datagen.flow_from_directory("resources/TRUNK12_test/Train", batch_size=8,
+train_generator = train_datagen.flow_from_directory("resources/TRUNK12_test/Train", batch_size=batch_size,
                                                     class_mode='categorical', target_size=(224, 224))
-validation_generator = test_datagen.flow_from_directory("resources/TRUNK12_test/Val", batch_size=8,
+validation_generator = test_datagen.flow_from_directory("resources/TRUNK12_test/Val", batch_size=batch_size,
                                                         class_mode='categorical', target_size=(224, 224))
 
 base_model = Sequential()
@@ -30,7 +32,7 @@ base_model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.001), loss='categorica
 resnet_history = base_model.fit(train_generator, validation_data=validation_generator,
                                 steps_per_epoch=16, epochs=100)
 
-base_model.save("ResNet50V1_12_64_64")
+base_model.save("ResNet50V1_12_32121")
 
 print('\nHistory dict: ', resnet_history.history)
 print("'loss': ", resnet_history.history['loss'])
--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 from tensorflow import keras
-from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
@@ -8,11 +8,11 @@ import os
 # Disable training on GPU
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
+image_path = "resources/prediction/oak_1.jpg"
+
 model = keras.models.load_model("ResNet50V1_12")
 
-image_path = "resources/prediction/oak_1.jpg"
-img = image.load_img(image_path)
-
+img = image.load_img(image_path, target_size=(224, 224))
 img_array = image.img_to_array(img)
 img_batch = np.expand_dims(img_array, axis=0)
 img_preprocessed = preprocess_input(img_batch)
