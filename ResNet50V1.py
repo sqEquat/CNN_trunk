8c06ec687ef9d6950e9b75e184ca14e43594f7c1
Equat <equati@yandex.ru>
2021-04-05 21:16:15 +0300

    Add resnet101 model function

--- ResNet50V1.py
+++ ResNet50V1.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 import csv
 # import os
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.applications import ResNet50, ResNet101
+# from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.applications.resnet import preprocess_input
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dropout, Dense
 from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
@@ -14,8 +15,9 @@ from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
 
 train_path = "resources/TRUNK12_test/Train"
 val_path = "resources/TRUNK12_test/Val"
-checkpoint_path = "models/resnet50/trunk12_{epoch:02d}_{val_acc:.4f}.h5"
-fit_result_csv = "stat/resnet50/040521.csv"
+
+checkpoint_path = "models/resnet50/resnet101_trunk12_{epoch:02d}_{val_acc:.4f}.h5"
+fit_result_csv = "stat/resnet50/040521_101.csv"
 
 img_shape = (224, 224)
 train_samples_num = 305
@@ -29,6 +31,14 @@ epochs = 100
 def resnet50_model():
     model = Sequential()
     model.add(ResNet50(include_top=False, pooling='avg', weights=None))
+    model.add(Dense(12, activation='softmax'))
+
+    return model
+
+
+def resnet101_model():
+    model = Sequential()
+    model.add(ResNet101(include_top=False, pooling='avg', weights=None))
     model.add(Dense(12, activation='softmax'))
 
     return model
@@ -49,13 +59,13 @@ def get_data_generators():
     return train, validation
 
 
-def training(model_base, train, val):
+def training(model_base, train_gen, val_gen):
     model_base.compile(optimizer=tf.keras.optimizers.Adam(lr=lr_rate), loss='categorical_crossentropy', metrics=['acc'])
     reduce_lr = ReduceLROnPlateau(monitor='val_acc', factor=0.5, patience=5, verbose=1, min_lr=0.000001)
     checkpoint = ModelCheckpoint(filepath=checkpoint_path, monitor='val_acc',
                                  verbose=1, save_best_only=True, mode='max')
 
-    history = base_model.fit(train, validation_data=val, steps_per_epoch=train_samples_num // batch_size,
+    history = model_base.fit(train_gen, validation_data=val_gen, steps_per_epoch=train_samples_num // batch_size,
                              validation_steps=val_samples_num // batch_size, epochs=epochs,
                              callbacks=[checkpoint, reduce_lr])
 
@@ -63,10 +73,9 @@ def training(model_base, train, val):
 
 
 if __name__ == '__main__':
+    train, val = get_data_generators()
     base_model = resnet50_model()
-    train_gen, val_gen = get_data_generators()
-
-    fit_history = training(base_model, train_gen, val_gen)
+    fit_history = training(base_model, train, val)
 
     print('\nHistory dict: ', fit_history.history)
     print("'loss': ", fit_history.history['loss'])
@@ -74,9 +83,7 @@ if __name__ == '__main__':
     print("'val_loss': ", fit_history.history['val_loss'])
     print("'val_acc': ", fit_history.history['val_acc'])
 
-    csv_columns = ['val_loss', 'val_acc']
-    with open(fit_result_csv, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-        writer.writeheader()
-        for data in fit_history.history:
-            writer.writerow(data)
+    with open(fit_result_csv, 'w') as output_csv:
+        writer = csv.writer(output_csv)
+        writer.writerow(fit_history.keys())
+        writer.writerows(zip(*fit_history.values()))
