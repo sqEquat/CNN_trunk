1281daf57b71f5e77c296b0664c624ecae44aac1
Equat <equati@yandex.ru>
2021-04-05 16:34:29 +0300

    Add prediction from generator and analyze of results

--- predict.py
+++ predict.py
@@ -1,31 +1,72 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import seaborn as sns
+import pandas as pd
 from tensorflow import keras
 from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.preprocessing import image
-import numpy as np
+from sklearn.metrics import classification_report
 import os
 
 
-# Disable training on GPU
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+# Not using GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-if __name__ == '__main__':
+image_path = "resources/prediction/alder_2.jpg"
+val_path = "resources/TRUNK12_test/Val"
+batch_size = 1
+img_shape = (224, 224)
+tree_types = ['Alder', 'Beech', 'Birch', 'Chestnut', 'Ginkgo biloba', 'Hornbeam', 'Horse chestnut',
+             'Linden', 'Oak', 'Oriental plane', 'Pine', 'Spruce']
 
-    image_path = "resources/prediction/alder_2.jpg"
-    batch_size = 10
-    img_shape = (224, 224)
 
-    model = keras.models.load_model("models/resnet50/trunk12_51_0.8140.h5")
-
-    img = image.load_img(image_path, target_size=(224, 224))
+def img_predict(path):
+    img = image.load_img(path, target_size=img_shape)
     img_array = image.img_to_array(img)
     img_batch = np.expand_dims(img_array, axis=0)
     img_preprocessed = preprocess_input(img_batch)
 
     prediction = model.predict(img_preprocessed)
 
-    tree_type = ['Alder', 'Beech', 'Birch', 'Chestnut', 'Ginkgo biloba', 'Hornbeam', 'Horse chestnut',
-                 'Linden', 'Oak', 'Oriental plane', 'Pine', 'Spruce']
-
     for i in range(len(prediction[0])):
-        tree = tree_type[i] + ': '
+        tree = tree_types[i] + ': '
         print(tree, prediction[0][i])
+
+    return prediction
+
+
+def generator_predict(path):
+    val_datagen = image.ImageDataGenerator(preprocessing_function=preprocess_input)
+    validation = val_datagen.flow_from_directory(val_path, batch_size=batch_size,
+                                                 class_mode='categorical', target_size=img_shape)
+    prediction = model.predict(validation)
+
+    return prediction, validation
+
+
+def pred_analyze(prediction, generator):
+    y_pred = np.argmax(prediction, axis=1)
+    con_mat = tf.math.confusion_matrix(generator.classes, y_pred).numpy()
+    con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
+    con_mat_df = pd.DataFrame(con_mat_norm,
+                              index=tree_types,
+                              columns=tree_types)
+
+    figure = plt.figure(figsize=(8, 8))
+    sns.heatmap(con_mat_df, annot=True, cmap=cm.Blues)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+    print('Classification Report')
+    print(classification_report(generator.classes, y_pred, target_names=tree_types))
+
+
+if __name__ == '__main__':
+
+    model = keras.models.load_model("models/resnet50/trunk12_51_0.8140.h5")
+    pred, gen = generator_predict(val_path)
+    pred_analyze(pred, gen)
